c4d6a89ae461d3dd679fea044fda4182e2b2991b
devuv <devuviswanath1996@gmail.com>
2023-05-29 00:06:38 -0400

    db connection

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const dbConnect = () => {
+  try {
+    const conn = mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB connected successfully");
+  } catch (error) {
+    throw new Error(error);
+    console.log("DB error");
+  }
+};
+module.exports = dbConnect;
