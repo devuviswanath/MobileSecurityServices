f6be40921b2a113209395912a79e97ab694e6e5c
devuv <devuviswanath1996@gmail.com>
2023-06-02 10:30:39 -0400

    nodemon

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = () => {
   try {
     const conn = mongoose.connect(process.env.MONGODB_URL);
-    console.log("DB connected successfully");
+    console.log(conn, "DB connected successfully");
   } catch (error) {
     throw new Error(error);
     console.log("DB error");
