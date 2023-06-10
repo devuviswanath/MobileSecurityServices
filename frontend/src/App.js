b4ebd7586b05eb08e659ef47c2e58a094dc77ff5
devuv <devuviswanath1996@gmail.com>
2023-06-10 01:17:21 -0400

    header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./componenets/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Cart from "./pages/Cart";
 import ContactUs from "./pages/ContactUs";
+import Checkout from "./pages/Checkout";
+import ContractPolicy from "./pages/ContractPolicy";
+import Faq from "./pages/Faq";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import OurStore from "./pages/OurStore";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Profile from "./pages/Profile";
+import ResetPassword from "./pages/ResetPassword";
+import Signup from "./pages/Signup";
+import SingleService from "./pages/SingleService";
+import TermsAndConditions from "./pages/TermsAndConditions";
 
 function App() {
   return (
@@ -13,8 +27,22 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<ContactUs />} />
+            <Route path="About" element={<About />} />
+            <Route path="Cart" element={<Cart />} />
+            <Route path="ContactUs" element={<ContactUs />} />
+            <Route path="Checkout" element={<Checkout />} />
+            <Route path="ContractPolicy" element={<ContractPolicy />} />
+            <Route path="Faq" element={<Faq />} />
+            <Route path="ForgotPassword" element={<ForgotPassword />} />
+            <Route path="Login" element={<Login />} />
+            <Route path="Orders" element={<Orders />} />
+            <Route path="OurStore" element={<OurStore />} />
+            <Route path="PrivacyPolicy" element={<PrivacyPolicy />} />
+            <Route path="Profile" element={<Profile />} />
+            <Route path="ResetPassword" element={<ResetPassword />} />
+            <Route path="Signup" element={<Signup />} />
+            <Route path="SingleService" element={<SingleService />} />
+            <Route path="TermsAndConditions" element={<TermsAndConditions />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/componenets/Footer.js
+++ frontend/src/componenets/Footer.js
@@ -1,7 +1,104 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsTwitter, BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Info</h4>
+              <div>
+                <address className="text-white fs-6">
+                  75 Watline Ave,
+                  <br />
+                  Mississauga,Toronto,
+                  <br />
+                  Ontario,Canada
+                </address>
+                <a
+                  href="tel: +1 4756478756"
+                  className="mt-4 d-block mb-2 text-white"
+                >
+                  +1 (475)-647-8756
+                </a>
+                <a
+                  href="mailto: mobilesecurityservices@gmail.com"
+                  className="mt-4 d-block mb-2 text-white"
+                >
+                  mobilesecurityservices@gmail.com
+                </a>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link to="/PrivacyPolicy" className="text-white py-2 mb-l">
+                  Privacy Policy
+                </Link>
+                <Link to="/ContractPolicy" className="text-white py-2 mb-l">
+                  Contract Policy
+                </Link>
+                <Link to="/TermsAndConditions" className="text-white py-2 mb-l">
+                  Terms and Conditions
+                </Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link to="/Faq" className="text-white py-2 mb-l">
+                  FAQ
+                </Link>
+                <Link to="/About" className="text-white py-2 mb-l">
+                  About Us
+                </Link>
+                <Link to="/ContactUs" className="text-white py-2 mb-l">
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Social Media</h4>
+              <div className="footer-link d-flex flex-column">
+                <Link
+                  className="text-white py-2 mb-l"
+                  to="https://www.facebook.com/"
+                >
+                  <BsFacebook className="fs-3" style={{ color: "white" }} />
+                </Link>
+                <Link
+                  className="text-white py-2 mb-l"
+                  to="https://twitter.com/home"
+                >
+                  <BsTwitter className="fs-3" style={{ color: "white" }} />
+                </Link>
+                <Link
+                  className="text-white py-2 mb-l"
+                  to="https://www.youtube.com/"
+                >
+                  <BsYoutube className="fs-3" style={{ color: "white" }} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className=".col-12">
+              <p className="text-center mb-0 text-white">
+                &copy;{new Date().getFullYear()}; ThirdEye limited
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
